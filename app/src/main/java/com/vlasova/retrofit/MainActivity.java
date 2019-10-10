818f93cf26dc5b3ec17047b472da3be4752b4d9c
Shaporeva <schaporeva.o@yandex.ru>
2019-10-10 09:51:01 +0300

    Retrofit & RX Проверка на соединение

--- app/src/main/java/com/vlasova/retrofit/MainActivity.java
+++ app/src/main/java/com/vlasova/retrofit/MainActivity.java
@@ -1,10 +1,13 @@
 package com.vlasova.retrofit;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -21,6 +24,26 @@ public class MainActivity extends AppCompatActivity {
     newsAdapter newsAdapter;
     Context context;
 
+    public static boolean hasConnection(final Context context)
+    {
+        ConnectivityManager cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifiInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (wifiInfo != null && wifiInfo.isConnected())
+        {
+            return true;
+        }
+        wifiInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        if (wifiInfo != null && wifiInfo.isConnected())
+        {
+            return true;
+        }
+        wifiInfo = cm.getActiveNetworkInfo();
+        if (wifiInfo != null && wifiInfo.isConnected())
+        {
+            return true;
+        }
+        return false;
+    }
 
     public ArrayList<HashMap<String, String>> listToArrayList (List<articles> list) {
         ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
@@ -42,38 +65,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         final Message[] s = {new Message()};
         setContentView(R.layout.activity_main);
+
         String url = "https://newsapi.org/";
         arrayListArticle = new ArrayList<>();
         context =  this;
 
+        if (hasConnection(context)) { //проверка на соединение
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(url)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .build();
 
+            MessagesAPI messagesAPI = retrofit.create(MessagesAPI.class);
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(url)
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build();
+            messagesAPI.messages()
+                    .subscribeOn(Schedulers.io()) //rx - нужна друшая реализация
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new DisposableSingleObserver<Message>() {
+                        @Override
+                        public void onSuccess(Message message) { //воврашеяется только один объект, и для него можно реализовать только onSuccess
+                            arrayListArticle = listToArrayList(message.getArticles());
+                            newsAdapter = new newsAdapter(context, arrayListArticle);
+                            ListView lst = (ListView) findViewById(R.id.listNews);
+                            lst.setAdapter(newsAdapter); // нужна отдельная реализация для отправки результата в адаптер (для вывода на экран)
+                            int i = 0;
+                        }
 
-        MessagesAPI messagesAPI = retrofit.create(MessagesAPI.class);
-
-        messagesAPI.messages()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new DisposableSingleObserver<Message>() {
-            @Override
-            public void onSuccess(Message message) {
-                arrayListArticle = listToArrayList(message.getArticles());
-                newsAdapter = new newsAdapter(context, arrayListArticle);
-                ListView lst = (ListView) findViewById(R.id.listNews);
-                lst.setAdapter(newsAdapter);
-                int i = 0;
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                int i = 0;
-            }
-        });
+                        @Override
+                        public void onError(Throwable e) {
+                            int i = 0;
+                        }
+                    });
+        } else {
+            Toast toast = Toast.makeText (getApplicationContext(), "Отсутствует соединение с Интернетом", Toast.LENGTH_SHORT);
+            toast.show();
+        }
 
 
 
@@ -99,4 +126,3 @@ public class MainActivity extends AppCompatActivity {
         });*/
     }
 }
-
--- app/src/main/java/com/vlasova/retrofit/MessagesAPI.java
+++ app/src/main/java/com/vlasova/retrofit/MessagesAPI.java
@@ -8,6 +8,5 @@ import retrofit2.http.GET;
 
 public interface MessagesAPI {
     @GET("v2/top-headlines?sources=google-news-ru&apiKey=4b1b5539788e4bf3be958892a276769f")
-    //Call <Message> messages();
     Single<Message> messages();
 }
